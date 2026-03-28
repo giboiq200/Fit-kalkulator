25e78f49f6ca0270d8192abff5088c4b08b61945
Bogdan <giboiq200@gmail.com>
2026-03-28 18:10:05 +0100

    popravljam analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Analytics>
+    <Analytics />
       <App />
-    </Analytics>
   </StrictMode>,
 )
